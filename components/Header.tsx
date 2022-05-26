e3adfc8e863d7c255e5a6302097841f600c653cd
AayushMohan <aayushmohan1702@gmail.com>
2022-05-26 17:19:04 +0530

    Added Search Box

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
-import { BeakerIcon, ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+} from '@heroicons/react/solid'
 import { StarIcon } from '@heroicons/react/outline'
 
 const Header = () => {
@@ -21,6 +26,17 @@ const Header = () => {
         <p className="ml-2 hidden flex-1 lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
       </div>
+
+      {/* Search Box */}
+      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
     </div>
   )
 }
