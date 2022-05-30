3bc3ba1b76784fff5d93e56edd6f5ecd6148c192
AayushMohan <aayushmohan1702@gmail.com>
2022-05-31 03:21:36 +0530

    Added PostBox Componenet

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,7 +1,25 @@
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const PostBox = () => {
-  return <div>PostBox</div>
+  const { data: session } = useSession()
+  return (
+    <form>
+      <div className="flex items-center space-x-3">
+        {/* Avatar */}
+        <input
+          disabled={!session}
+          className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
+          type="text"
+          placeholder={
+            session
+              ? 'Create a post by entering a title!'
+              : 'Sign in to create a post!'
+          }
+        />
+      </div>
+    </form>
+  )
 }
 
 export default PostBox
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       {/* PostBox */}
+      <PostBox />
 
       <div>{/* Feed */}</div>
     </div>
