e8fbf7d5b72f80b6a47884f8b2b1ac5880bae110
AayushMohan <aayushmohan1702@gmail.com>
2022-05-31 04:49:39 +0530

    Added Form Data

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,10 +1,19 @@
 import { LinkIcon, PhotographIcon } from '@heroicons/react/outline'
 import { useSession } from 'next-auth/react'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import Avatar from './Avatar'
 
 const PostBox = () => {
   const { data: session } = useSession()
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>()
+
   return (
     <form className="sticky top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
       <div className="flex items-center space-x-3">
