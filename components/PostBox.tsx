e3aa4b63a02eb87bcde757aefe6d8ba83d8642ef
AayushMohan <aayushmohan1702@gmail.com>
2022-05-31 05:39:42 +0530

    Added Body

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -43,11 +43,10 @@ const PostBox = () => {
       </div>
 
       {!!watch('postTitle') && (
-        <div>
+        <div className="flex flex-col py-2">
           {/* Body */}
-          <div>
-            <p>Body</p>
-
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
             <input
               className="m-2 flex-1 bg-blue-50 p-2 outline-none"
               {...register('postBody')}
