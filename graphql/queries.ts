f5f4eaa27afe70c290b76dfd147365e5dd38c49a
AayushMohan <aayushmohan1702@gmail.com>
2022-06-13 18:55:35 +0530

    Added Votes and Subreddit

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,4 +1,19 @@
-import { gql } from "@apollo/client";
+import { gql } from "@apollo/client"
+
+
+export const GET_ALL_POSTS = gql`
+  query MyQuery {
+    getPostList{ 
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+    }
+  }
+`
 
 
 export const GET_SUBREDDIT_BY_TOPIC = gql`
